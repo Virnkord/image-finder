b62f5c698ca8326d43ea0b5754b1b1299f3a3812
Evgeny <simplicityparadox@gmail.com>
2021-05-16 23:10:10 +0300

    test(all): added download and path tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,7 +2,27 @@ import time
 import sys,os
 import pytest
 sys.path.append(os.path.join(os.path.dirname(__file__),os.pardir,"image_finder"))
+print(sys.path)
 from img_downloader.img_scraper import search_ddg, search_bing, search_google, search_unsplash
+from image_finder.image_finder import search_images
+
+def test_download():
+    ak = "Kha6jgNu2s09zWWSw0UK6eA3OhlkmYU2V-D4Fs-MSiA"
+    search = "nature"
+    root = os.path.join("test_download", "unsplash")
+    cwd = os.getcwd()
+    path = os.path.join(cwd,root)
+    if os.path.exists(path):
+        for root, dirs, files in os.walk(path, topdown=False):
+            for name in files:
+                os.remove(os.path.join(root, name))
+            for name in dirs:
+                os.rmdir(os.path.join(root, name))
+        os.rmdir(path)
+    _, _ = search_unsplash(search, 1, root, ak)
+    time.sleep(2)
+    _, _, filenames = next(os.walk(path))
+    assert os.path.isfile(os.path.join(cwd,root,filenames[0]))
 
 def test_search_unsplash():
     ak = "Kha6jgNu2s09zWWSw0UK6eA3OhlkmYU2V-D4Fs-MSiA"
@@ -17,10 +37,12 @@ def test_search_unsplash():
             for name in dirs:
                 os.rmdir(os.path.join(root, name))
         os.rmdir(path)
-    search_unsplash(search, 1, root, ak)
+    _, url = search_unsplash(search, 1, root, ak)
     time.sleep(2)
-    _, _, filenames = next(os.walk(path))
-    assert os.path.isfile(os.path.join(cwd,root,filenames[0]))
+    print (url)
+    assert len(url) != 0
+    # _, _, filenames = next(os.walk(path))
+    # assert os.path.isfile(os.path.join(cwd,root,filenames[0]))
 
 def test_search_ddg():
     search = "duck"
@@ -34,13 +56,13 @@ def test_search_ddg():
             for name in dirs:
                 os.rmdir(os.path.join(root, name))
         os.rmdir(path)
-    search_ddg(search, 1, root)
+    _, url = search_ddg(search, 1, root)
     time.sleep(2)
     _, _, filenames = next(os.walk(path))
-    print(filenames)
-    print(cwd)
-    print(root)
-    assert os.path.isfile(os.path.join(cwd,root,filenames[0]))
+    print (url)
+    assert len(url) != 0
+    # _, _, filenames = next(os.walk(path))
+    # assert os.path.isfile(os.path.join(cwd,root,filenames[0]))
     
 def test_search_bing():
     search = "microsoft"
@@ -54,10 +76,13 @@ def test_search_bing():
             for name in dirs:
                 os.rmdir(os.path.join(root, name))
         os.rmdir(path)
-    search_bing(search, 1, root)
+    _, url = search_bing(search, 1, root)
     time.sleep(2)
     _, _, filenames = next(os.walk(path))
-    assert os.path.isfile(os.path.join(cwd,root,filenames[0]))
+    print (url)
+    assert len(url) != 0
+    # _, _, filenames = next(os.walk(path))
+    # assert os.path.isfile(os.path.join(cwd,root,filenames[0]))
     
 def test_search_google():
     search = "penguin"
@@ -71,7 +96,27 @@ def test_search_google():
             for name in dirs:
                 os.rmdir(os.path.join(root, name))
         os.rmdir(path)
-    search_google(search, 1, root)
+    _, url = search_google(search, 1, root)
     time.sleep(2)
     _, _, filenames = next(os.walk(path))
-    assert os.path.isfile(os.path.join(cwd,root,filenames[0]))
+    print (url)
+    assert len(url) != 0
+    # _, _, filenames = next(os.walk(path))
+    # assert os.path.isfile(os.path.join(cwd,root,filenames[0]))
+    
+def test_path_collector():
+    root = os.path.join("test_download", "google")
+    cwd = os.getcwd()
+    path = os.path.join(cwd,root)
+    if os.path.exists(path):
+        for root, dirs, files in os.walk(path, topdown=False):
+            for name in files:
+                os.remove(os.path.join(root, name))
+            for name in dirs:
+                os.rmdir(os.path.join(root, name))
+        os.rmdir(path)
+    images = search_images(["nature"], 1, root, None, ["google"])
+    time.sleep(2)
+    path = list(images.values())[0][0]
+    print(path)
+    assert os.path.isabs(path)
