2b21555ed5c9afe748b9382286eaca4b94032475
Evgeny <simplicityparadox@gmail.com>
2021-05-31 00:01:40 +0300

    chore(cli): help text rewritten

--- image_finder/image_finder.py
+++ image_finder/image_finder.py
@@ -41,22 +41,22 @@ def filter_images(images):
 if __name__ == "__main__":
     import argparse
 
+    available_engines = ["unsplash", "google", "bing", "duckduckgo", "all"]
     parser = argparse.ArgumentParser()
     parser.add_argument('-k', '--keywords',
-                        help='delimited list input', type=str, required=True)
-    parser.add_argument('-o', '--output_directory', help='download images in a specific main directory', type=str,
+                        help='keywords for search and filtering (ex.: "car, cat, dog")', type=str, required=True)
+    parser.add_argument('-o', '--output_directory', help='download images in a specific main directory (by default /downloads)', type=str,
                         required=False)
     parser.add_argument('-c', '--class_directory', help='download images in a specific sub-directory', action='store_true',
                         required=False)
-    parser.add_argument('-l', '--limit', help='number', type=int, required=False)
-    parser.add_argument('-e', '--engine', help='delimited list input', type=str, required=False)
+    parser.add_argument('-l', '--limit', help='number of images to download from each source', type=int, required=False)
+    parser.add_argument('-e', '--engine', help=f'list of engines (ex.: "google, bing" or "all")\navailable enginges: {available_engines}', type=str, required=False)
     parser.add_argument('-nf', '--nofilter', help='disable filtering', action='store_true', required=False)
 
     args = parser.parse_args()
     arguments = vars(args)
     search_keywords = [str(item) for item in arguments['keywords'].replace(" ", "").split(',')]
     limit = arguments['limit']
-    available_engines = ["unsplash", "google", "bing", "duckduckgo", "all"]
     if arguments['engine']:
         if arguments['engine'] == "all":
             search_engines = available_engines
@@ -77,7 +77,6 @@ if __name__ == "__main__":
 
     class_directory = True if arguments['class_directory'] else None
     filtering = not arguments['nofilter']
-    print(search_keywords)
     images = search_images(search_keywords, limit, output_directory, class_directory, search_engines)
     if filtering:
         filter_images(images)
