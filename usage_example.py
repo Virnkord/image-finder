d2b6bd342bb41f02d2efa2f171ff01b2c736cf3f
Evgeny <simplicityparadox@gmail.com>
2021-05-31 00:03:10 +0300

    chore(usage_example): different example

--- usage_example.py
+++ usage_example.py
@@ -1,6 +1,17 @@
 from image_finder.image_finder import *
-images = search_images(["dog"], 5, "downloads", True, ["google"])
+# images = search_images(["boat", "car", "glasses", "horse","cat","dog", "sculpture", "fish"], 3, "downloads", True, ["bing"])
+images = search_images(["dog"], 2, "downloads", True, ["unsplash", "google", "bing", "duckduckgo"])
 filter_images(images)
+
+
+
+
+
+
+
+
+
+
 
 # import pickle
 # lb = pickle.loads(open(r"D:\\Development\\oidv6\\lb-20210515.pickle", 'rb').read())
